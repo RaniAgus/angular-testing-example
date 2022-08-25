fad136824f55e599ba4bbf4c6549bbace51e47bf
Agustin Ranieri <aguseranieri@gmail.com>
2022-08-25 12:43:46 -0300

    tests: set input value before each test

--- src/app/users/user-list/user-list.component.spec.ts
+++ src/app/users/user-list/user-list.component.spec.ts
@@ -2,9 +2,10 @@ import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 import { UserListComponent } from './user-list.component';
+import { User } from 'src/app/shared/models/user';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
@@ -25,6 +26,7 @@ describe('UserListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
+    component.dataSource = new MatTableDataSource<User>([]);
     fixture.detectChanges();
   });
 
