10ce895b09242e896aac81b79c883c3b701a188d
Agustin Ranieri <aguseranieri@gmail.com>
2022-08-25 09:18:13 -0300

    feat: fetch users from usersService

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,31 +1,50 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { take } from 'rxjs';
 import { User } from './user';
-import { UsersDataSource } from './users-datasource';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements AfterViewInit {
+export class UsersComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatTable) table!: MatTable<User>;
-  dataSource: UsersDataSource;
 
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name', 'username', 'email', 'website', 'company'];
+  dataSource: MatTableDataSource<User> = new MatTableDataSource<User>([]);
+  displayedColumns = [
+    'id',
+    'name',
+    'username',
+    'email',
+    'website',
+    'company',
+  ];
 
-  constructor() {
-    this.dataSource = new UsersDataSource();
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit(): void {
+    this.usersService.getAllUsers()
+      .pipe(take(1))
+      .subscribe(data => this.dataSource.data = data);
   }
 
   ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.dataSource.sortingDataAccessor = this.sortingDataAccessor;
     this.table.dataSource = this.dataSource;
+  }
+
+  sortingDataAccessor(data: User, property: string): string | number {
+    switch(property) {
+      case 'company': return data.company.name;
+      default: return (data as any)[property];
+    }
   }
 }
